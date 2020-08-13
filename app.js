aaeaa9a7048542d6155fc8168f670c03fc231183
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2020-08-13 18:48:29 +0300

    Get fieldname and attach Event listener

--- app.js
+++ app.js
@@ -63,3 +63,19 @@ function checkPasswordsMatch(input1, input2) {
         showError(input2, 'Passwords do not match');
     }
 }
+
+// Get fieldname
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+// Event listeners
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordsMatch(password, password2);
+});
